9685d3cc53de5a418cbc1e06bd3243ef151974c4
Jose Ayerdis <joseayerdis@gmail.com>
2016-04-13 04:33:01 -0700

    Create example Image componenet

--- app/app.js
+++ app/app.js
@@ -1,4 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Image from './image'
 
 ReactDOM.render(<div>Sampleapp</div>, document.body);
---
+++ app/image.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default class Image extends React.Component {
+
+	render() {
+		return <img src="https://scontent.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/917384_587085891468319_344695119_n.jpg?ig_cache_key=MTIyNzQ3ODYzOTY2ODIyMjY4NA%3D%3D.2.l"/>
+	}
+
+}
